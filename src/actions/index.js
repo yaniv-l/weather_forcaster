7dcc35266093ca4c4366eae66905f2802f441307
yaniv-l <yanivl@srl.co.il>
2016-08-06 15:34:03 +0300

    added ActionCreator and implement a call to openweathermap using axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,19 @@
+import axios from 'axios';
+
 const API_KEY = "6f3b40c906ff44dca916211c6368a9d6";
+
+// Construct the root URL fot wweather api using template strings
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+  // Construct the final URL fot weather api using template strings
+  const url = `${ROOT_URL}&q=${city},il`;
+  const request = axios.get(url);
+
+  return{
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
